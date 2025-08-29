c6662e288220bde1cdd67b52098f55c5a1ca2eee
Younes <younes.bouhouche12@gmail.com>
2025-08-29 22:52:54 +0100

    refactor: remove unused useEffect import in useQueryFetch hook

--- src/hooks/useQueryFetch.tsx
+++ src/hooks/useQueryFetch.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function useQueryFetch<T>(
   endpoint: string,
